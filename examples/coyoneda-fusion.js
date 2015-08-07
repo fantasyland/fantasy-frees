323f8697fb0dc3e57b9d7099efed8c862f15d7f0
SimonRichardson <stickupkid@gmail.com>
2015-08-07 21:55:16 +0100

    Coyoneda fusion

---
+++ examples/coyoneda-fusion.js
@@ -0,0 +1,23 @@
+var fantasia = require('./../fantasia'),
+
+    Coyoneda = fantasia.Coyoneda;
+
+function add(x) {
+    return x + 1;
+}
+
+function toString(x) {
+    return '' + x;
+}
+
+(function(){
+
+    var arr = [1, 2, 3, 4, 6, 7, 8, 9],
+        coyo = Coyoneda.lift(arr),
+        res = coyo
+            .map(add)
+            .map(toString);
+
+    console.log(res.lower());
+
+})();
