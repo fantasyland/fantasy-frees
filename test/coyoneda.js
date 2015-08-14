475f321dc1383a850da881092c7299fe1dfff909
SimonRichardson <stickupkid@gmail.com>
2015-08-14 11:57:28 +0100

    Adding a test for coyoneda

---
+++ test/coyoneda.js
@@ -0,0 +1,24 @@
+var λ           = require('fantasy-check/src/adapters/nodeunit'),
+    functor     = require('fantasy-check/src/laws/functor'),
+
+    fantasia    = require('../fantasia'),
+
+    Identity = require('fantasy-identities'),
+
+    Coyoneda = fantasia.Coyoneda;
+ 
+function of(x) {
+    return Coyoneda.lift(Identity.of(x));
+}
+
+function run(x) {
+    return x.lower();
+}
+
+exports.coyoneda = {
+
+    // Functor tests
+    'All (Functor)': functor.laws(λ)(of, run),
+    'Identity (Functor)': functor.identity(λ)(of, run),
+    'Composition (Functor)': functor.composition(λ)(of, run)
+};
